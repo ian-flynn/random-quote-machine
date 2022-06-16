55a3c1eff4b1f707484421fae355e8bec9dc3c4f
ianflynnwork <ianflynnwork@gmail.com>
2022-06-16 11:57:39 -0400

    jj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+  const [quotes, setQuotes] = useState([]);
+  const [randomQuote, setRandomQuote] = useState([]);
+
+  useEffect(() => {
+    async function fetchData(){
+        const response = await fetch('https://type.fit/api/quotes');
+        const data = await response.json();
+
+        setQuotes(data);
+        let randomIndex = Math.floor(Math.random() * data.length);
+        setRandomQuote(data[randomIndex]);
+    }
+  fetchData();
+}, []);
+
+const newQuote = () => {
+  let randomIndex = Math.floor(Math.random() * quotes.length);
+        setRandomQuote(quotes[randomIndex]);
+}
+
+return (
+  <div id="wrapper">
+    <div className="background-box"></div>
+    <div id="quote-box">
+      <div className="quote-area">
+        <div className="shape"></div>
+        <div id="text">{randomQuote.text}</div>
+        <div id="author">{randomQuote.author || 'No Author'}</div> 
+      </div>
+      <div className="button-area">
+        <button className="button shift" id='new-quote' onClick={ newQuote }>New</button>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="button"
+          href={`https://twitter.com/intent/tweet?text=\"${randomQuote.text}\" \- ${randomQuote.author}`}
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          id='tweet-quote'><FontAwesomeIcon icon={faTwitter} className="bird" /></a>
+          
+      </div>
+    </div> 
+  </div>
+    
+);
 }
 
 export default App;
